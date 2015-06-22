f5461b6424e32ab0bdc59d1b1c6b0199f2d8103a
Robert Deutz <rdeutz@googlemail.com>
2015-06-22 20:50:35 +0200

    just some basic files and folders

---
+++ code/libraries/babioon/library.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Babioon
+ * @subpackage  Library
+ *
+ * @copyright   Copyright (C) 2013 Roberto Segura. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+// non supported PHP version detection. EJECT! EJECT! EJECT!
+if(version_compare(PHP_VERSION, '5.4.0', '<'))
+{
+	return JError::raise(E_ERROR, 500, 'PHP versions less than 5.4.0 are not supported.<br/><br/>');
+}
+
+// Ensure that autoloaders are set
+JLoader::setup();
+
+//require_once __DIR__ . '/vendor/autoload.php';
+
+// Global libraries autoloader
+//JLoader::registerPrefix('Sample', dirname(__FILE__));
+
+// Common fields
+JFormHelper::addFieldPath(dirname(__FILE__) . '/form/field');
+
+// Common form rules
+//JFormHelper::addRulePath(dirname(__FILE__) . '/form/rule');
+
+// Common HTML helpers
+//JHtml::addIncludePath(dirname(__FILE__) . '/html');
+
+// Load library language
+// $lang = JFactory::getLanguage();
+// $lang->load('lib_conferenceplus', __DIR__);
+
